eac08d73be971cf04f485abb35827b4f023586e4
Lestoy <51046689+linxinyu123@users.noreply.github.com>
2025-07-18 19:04:31 +0100

    666

---
+++ src/extensions/types.ts
@@ -0,0 +1,60 @@
+import { ReactNode } from "react";
+
+interface CommonExtensionData {
+  id: string;
+  name: {
+    en: string;
+  };
+  description: {
+    en: string;
+  };
+  logo: {
+    light: {
+      source: string;
+    };
+    dark: {
+      source: string;
+    };
+  };
+  installed?: boolean;
+  disabled?: boolean;
+}
+
+interface AppExtensionData extends CommonExtensionData {
+  type: "APP";
+  kind: "OFFICIAL" | "OSS";
+  manifestUrl: string | null;
+  repositoryUrl: string | null; // Typo in the original code
+  isCustomApp?: boolean;
+  appId?: string;
+}
+
+interface PluginExtensionData extends CommonExtensionData {
+  type: "PLUGIN";
+}
+
+export type ExtensionData = AppExtensionData | PluginExtensionData;
+
+export type ExtensionGroup = "payments" | "taxes" | "cms" | "automation";
+
+export type ExtensionsGroups = Record<ExtensionGroup, { title: string; items: ExtensionData[] }>;
+
+export type APIExtensionsResponse = Array<{
+  id: string;
+  name: { en: string };
+  extensions: ExtensionData[];
+}>;
+
+/*
+  Candidate for refactoring. InstalledExtension is only one case.
+  We have also pending installation, failed installation, etc. and they have different actions.
+  We should create dedicated types for each case (PendingInstallation, FailedInstallation and so on)
+*/
+export type InstalledExtension = {
+  id: string;
+  name: string;
+  logo: ReactNode;
+  info: ReactNode;
+  href?: string;
+  actions?: ReactNode;
+};
